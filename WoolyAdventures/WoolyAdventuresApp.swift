aec803f0c8db6b2cfe9de0277ca9db0b54c60254
Elisabeth Manalo <eliman24@bergen.org>
2023-08-04 04:12:57 -0400

    Initial Commit

---
+++ WoolyAdventures/WoolyAdventuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  WoolyAdventuresApp.swift
+//  WoolyAdventures
+//
+//  Created by Elisabeth Manalo on 8/4/23.
+//
+
+import SwiftUI
+
+@main
+struct WoolyAdventuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
